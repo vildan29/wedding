b57aa5cc18760744bdfdc5134d463292e70598f4
Vildan29 <vildan.oezguer@htwg-konstanz.de>
2014-05-24 16:23:28 +0200

    verändert

--- target/scala-2.10/src_managed/main/views/html/index.template.scala
+++ target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -28,7 +28,7 @@ object index extends BaseScalaTemplate[play.api.templates.HtmlFormat.Appendable,
 
 Seq[Any](format.raw/*1.19*/("""
 
-"""),_display_(Seq[Any](/*3.2*/main("Welcome to Play")/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
+"""),_display_(Seq[Any](/*3.2*/main("Welcome to Test")/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
 
     """),_display_(Seq[Any](/*5.6*/play20/*5.12*/.welcome(message, style = "Java"))),format.raw/*5.45*/("""
 
@@ -45,9 +45,9 @@ Seq[Any](format.raw/*1.19*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Sat May 24 00:17:26 CEST 2014
+                    DATE: Sat May 24 00:31:06 CEST 2014
                     SOURCE: /Users/Vildan/git/wedding/app/views/index.scala.html
-                    HASH: c1c7a8094ff6fdf7b189be1a11ff9a14918bbdfd
+                    HASH: 401b78a2877bede7ee6c5b5abdc90674c3e97f44
                     MATRIX: 774->1|885->18|922->21|953->44|992->46|1033->53|1047->59|1101->92|1134->95
                     LINES: 26->1|29->1|31->3|31->3|31->3|33->5|33->5|33->5|35->7
                     -- GENERATED --
